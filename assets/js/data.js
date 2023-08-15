15c217d6f6a71cb7dbfdda1453bc79537c221fa8
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-08-15 09:33:32 +0630

    data js updated

---
+++ assets/js/data.js
@@ -0,0 +1,14 @@
+const popular_course_wrappe = document.querySelector('.popular-course-wrapper');
+
+
+let courses = [
+    {'id': 1, 'name': 'Core Java', 'short-desc': 'Complete Java Full Course', 'price': 89},
+    {'id': 2, 'name': 'Python Beginner', 'short-desc': 'Python Course for Beginner', 'price': 'Free'},
+    {'id': 3, 'name': 'JavaScript Course', 'short-desc': 'JavaScript Full Course', 'price': 69},
+    {'id': 4, 'name': 'PHP Tutorial', 'short-desc': 'PHP programming Language', 'price': 89},
+    {'id': 5, 'name': 'Web Development', 'short-desc': 'Web Development Bootcamp', 'price': 199},
+];
+
+
+
+
