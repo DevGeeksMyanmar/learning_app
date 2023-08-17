99c1b56a51b35aae88be8f66b79b1c01b4f8d76f
Zed <99192842+Hein-HtetSan@users.noreply.github.com>
2023-08-17 08:09:23 +0630

    updated stuffs

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,19 +1,6 @@
-let courses = [
-    {'id': 1, 'name': 'Core Java', 'short_desc': 'Complete Java Full Course', 'price': 89, 'image': '../assets/pic/Java-BCG-Banner.png'},
-    {'id': 2, 'name': 'Python Beginner', 'short_desc': 'Python Course for Beginner', 'price': 'Free', 'image': '../assets/pic/FREE-Python-Course-For-Beginners.png'},
-    {'id': 3, 'name': 'JavaScript Course', 'short_desc': 'JavaScript Full Course', 'price': 69, 'image': '../assets/pic/JavaScript-BCG-Banner-icons.png'},
-    {'id': 4, 'name': 'PHP Tutorial', 'short_desc': 'PHP programming Language', 'price': 89, 'image': '../assets/pic/skill-guide-php.png'},
-    {'id': 5, 'name': 'Web Development', 'short_desc': 'Web Development Bootcamp', 'price': 199, 'image': '../assets/pic/BCG-Web-Development-NEW-Banner.png'},
-];
 
-let l_courses = [
-    {'id': 1, 'name': 'Core Java', 'short_desc': 'Complete Java Full Course', 'price': 89, 'image': '../assets/pic/Java-BCG-Banner.png'},
-    {'id': 2, 'name': 'Python Beginner', 'short_desc': 'Python Course for Beginner', 'price': 'Free', 'image': '../assets/pic/FREE-Python-Course-For-Beginners.png'},
-    {'id': 3, 'name': 'JavaScript Course', 'short_desc': 'JavaScript Full Course', 'price': 69, 'image': '../assets/pic/JavaScript-BCG-Banner-icons.png'},
-    {'id': 4, 'name': 'PHP Tutorial', 'short_desc': 'PHP programming Language', 'price': 89, 'image': '../assets/pic/skill-guide-php.png'},
-    {'id': 5, 'name': 'Web Development', 'short_desc': 'Web Development Bootcamp', 'price': 199, 'image': '../assets/pic/BCG-Web-Development-NEW-Banner.png'},
-];
-
+import { courses } from "./api.js";
+import { l_courses } from "./api.js";
 
 const popular_course_wrapper = document.querySelector('.popular-course-wrapper');
 const latest_course_wrapper = document.querySelector(".latest-course-wrapper");
