c8f03170bb452e03220ff17671da2ce174d09e71
Zed <99192842+Hein-HtetSan@users.noreply.github.com>
2023-08-17 19:01:25 +0630

    delete api and update into data and details js file

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,10 +1,25 @@
+let courses = [
+    {'id': 1, 'name': 'Core Java', 'short_desc': 'Complete Java Full Course', 'price': 89.0, 'image': '../assets/pic/Java-BCG-Banner.png'},
+    {'id': 2, 'name': 'Python Beginner', 'short_desc': 'Python Course for Beginner', 'price': 'Free', 'image': '../assets/pic/FREE-Python-Course-For-Beginners.png'},
+    {'id': 3, 'name': 'JavaScript Course', 'short_desc': 'JavaScript Full Course', 'price': 69.0, 'image': '../assets/pic/JavaScript-BCG-Banner-icons.png'},
+    {'id': 4, 'name': 'PHP Tutorial', 'short_desc': 'PHP programming Language', 'price': 89.0, 'image': '../assets/pic/skill-guide-php.png'},
+    {'id': 5, 'name': 'Web Development', 'short_desc': 'Web Development Bootcamp', 'price': 199.0, 'image': '../assets/pic/BCG-Web-Development-NEW-Banner.png'},
+];
 
-import { courses } from "./api.js";
-import { l_courses } from "./api.js";
+let l_courses = [
+    {'id': 1, 'name': 'Core Java', 'short_desc': 'Complete Java Full Course', 'price': 89.0, 'image': '../assets/pic/Java-BCG-Banner.png'},
+    {'id': 2, 'name': 'Python Beginner', 'short_desc': 'Python Course for Beginner', 'price': 'Free', 'image': '../assets/pic/FREE-Python-Course-For-Beginners.png'},
+    {'id': 3, 'name': 'JavaScript Course', 'short_desc': 'JavaScript Full Course', 'price': 69.0, 'image': '../assets/pic/JavaScript-BCG-Banner-icons.png'},
+    {'id': 4, 'name': 'PHP Tutorial', 'short_desc': 'PHP programming Language', 'price': 89.0, 'image': '../assets/pic/skill-guide-php.png'},
+    {'id': 5, 'name': 'Web Development', 'short_desc': 'Web Development Bootcamp', 'price': 199.0, 'image': '../assets/pic/BCG-Web-Development-NEW-Banner.png'},
+];
+
+
 
 const popular_course_wrapper = document.querySelector('.popular-course-wrapper');
 const latest_course_wrapper = document.querySelector(".latest-course-wrapper");
 const nav_links = document.querySelectorAll(".nav-link");
+const card = document.querySelectorAll(".card-img");
 
 
 
@@ -13,7 +28,7 @@ const nav_links = document.querySelectorAll(".nav-link");
 const uplode_pp_course = (items) => {
     const pp_courses = items.map(e => {
         return `
-            <a href="" class="p-card position-relative swiper-slide d-flex flex-column align-items-center justify-content-start p-2">
+            <div class="p-card position-relative swiper-slide d-flex flex-column align-items-center justify-content-start p-2">
                 <div class="card-img mb-2" 
                 style="background: url(${e.image});
                 background-position: center;
@@ -32,7 +47,7 @@ const uplode_pp_course = (items) => {
 
                 <!-- badge  -->
                 <div class="badge"><img src="../assets/svg/star-solid.svg" alt=""></div>
-            </a>
+            </div>
         `
     })
     .join(" ");
@@ -42,23 +57,53 @@ uplode_pp_course(courses);
 
 
 // upload latest course
-const upload_latest_course = (items) => {
-    const upload_course = items.map(e => {
-        return `
-        <div class="col-12 col-md-6 col-lg-4 mb-4">
-            <a href="" class="p-card position-relative swiper-slide d-flex flex-column align-items-center justify-content-start p-2">
+// const upload_latest_course = (items) => {
+//     const upload_course = items.map(e => {
+//         return `
+//         <div class="col-12 col-md-6 col-lg-4 mb-4">
+//             <div class="p-card latest_course_card position-relative swiper-slide d-flex flex-column align-items-center justify-content-start p-2">
+//                 <div class="card-img mb-2" 
+//                     style="background: url(${e.image});
+//                     background-position: center;
+//                     background-repeat: no-repeat;
+//                     background-size: cover;"></div>
+//                 <div class="w-100 card-content d-flex align-items-center justify-content-between">
+//                     <div class="content d-flex flex-column align-items-start justify-content-center">
+//                         <h5>${e.name}</h5>
+//                         <span>${e.short_desc}</span>
+//                     </div>
+//                 <div class="price">
+//                     <span>$${e.price}</span>
+//                 </div>
+//             </div>
+
+//             <!-- badge  -->
+//             <div class="latest-badge">Latest</div>
+//         </div>
+//     </div>
+//         `;
+//     })
+//     .join(" ");
+//     latest_course_wrapper.innerHTML = upload_course;
+// }
+// upload_latest_course(l_courses);
+
+for(let i=0; i<l_courses.length; i++){
+    const course = `
+    <div class="col-12 col-md-6 col-lg-4 mb-4">
+            <a href="./detail.html?id=${l_courses[i].id}" class="p-card latest_course_card position-relative swiper-slide d-flex flex-column align-items-center justify-content-start p-2">
                 <div class="card-img mb-2" 
-                    style="background: url(${e.image});
+                    style="background: url(${l_courses[i].image});
                     background-position: center;
                     background-repeat: no-repeat;
                     background-size: cover;"></div>
                 <div class="w-100 card-content d-flex align-items-center justify-content-between">
                     <div class="content d-flex flex-column align-items-start justify-content-center">
-                        <h5>${e.name}</h5>
-                        <span>${e.short_desc}</span>
+                        <h5>${l_courses[i].name}</h5>
+                        <span>${l_courses[i].short_desc}</span>
                     </div>
                 <div class="price">
-                    <span>$${e.price}</span>
+                    <span>$${l_courses[i].price}</span>
                 </div>
             </div>
 
@@ -66,12 +111,9 @@ const upload_latest_course = (items) => {
             <div class="latest-badge">Latest</div>
         </a>
     </div>
-        `;
-    })
-    .join(" ");
-    latest_course_wrapper.innerHTML = upload_course;
+    `;
+    latest_course_wrapper.innerHTML += course;
 }
-upload_latest_course(l_courses);
 
 // navbar link
 nav_links.forEach(e => {
@@ -84,3 +126,13 @@ nav_links.forEach(e => {
         e.classList.add('active');
     })
 })
+
+// Assuming latest_course_wrapper is a valid reference to the wrapper element
+latest_course_wrapper.addEventListener('click', (event) => {
+    const clickedElement = event.target.closest('.latest_course_card');
+    
+    if (clickedElement) {
+        // Handle the click event here
+        console.log('Card clicked:', clickedElement.querySelector('h5').textContent);
+    }
+});
--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,1 +1,392 @@
-import { course_detail } from "./api,js";
+let course_detail = [
+    // java course details
+    {
+        "id": 1,
+        "course_name": "Core Java",
+        "course_label": "Complete java full course",
+        "course_image": "../assets/pic/Java-BCG-Banner.png",
+        "prices": 89.0,
+        "descp": `
+            Learn the Skills Needed to be a Java Programmer.
+            Quickly master the Java Programming language
+            and the packages that constitute its rich
+            set of core libraries.
+        `,
+        "learn": [
+            "Learn the basic syntax and function of the java programming language",
+            "Apply object-oriented programming technique to building classes, creating objects, and understanding how solutions are packaged in java",
+            "Learn how to implement inheritance and polymorphism in java",
+            "Use selected parts of the vast Java SE class library to enhance your java programming technique"
+        ],
+        "modules": [
+            "Introduction to Java Programming",
+            "Java Languages Fundamental - Types",
+            "Java Languages Fundamental - Flows of Control",
+            "Classes and Objects",
+            "Creating Classes, Objects and using it"
+        ],
+        "instructors": [
+            {
+                "name": "instructor 1",
+                "image": ""
+            },
+            {
+                "name" : "instructor 2",
+                "image": ""
+            }
+        ]
+    },
+
+    // python course details
+    {
+        "id": 2,
+        "course_name": "Python Course",
+        "course_label": "Crash Course on Python",
+        "course_image": "../assets/pic/Java-BCG-Banner.png",
+        "prices": 199.0,
+        "descp": `
+                This course is designed to teach you 
+                the foundations in order to write simple 
+                programs in Python using the most common 
+                structures. No previous exposure to 
+                programming is needed. By the end of 
+                this course, you'll understand the 
+                benefits of programming in IT roles; 
+                be able to write simple programs using 
+                Python.
+        `,
+        "learn": [
+            "What Python is and why Python is relevant to automation",
+            "How to use the basic Python structures: strings, lists, and dictionaries",
+            "How to write short Python scripts to perform automated actions",
+            "How to create your own Python objects"
+        ],
+        "modules": [
+            "Hello Python",
+            "Basic Python Syntax",
+            "Loops",
+            "Strings, Lists and Dictionaries",
+            "Object Oriented Programming (Optional)",
+            "Final Project"
+        ],
+        "instructors": [
+            {
+                "name": "Paul Resnick",
+                "image": ""
+            },
+            {
+                "name" : "Steve Oney",
+                "image": ""
+            }
+        ]
+    },
+
+    // javaScripts course details
+    {
+        "id": 3,
+        "course_name": "JavaScript Course",
+        "course_label": "JavaScript, jQuery, and JSON",
+        "course_image": "../assets/pic/JavaScript-BCG-Banner-icons.png",
+        "prices": 89.0,
+        "descp": `
+                In this course, we'll look at the 
+                JavaScript language, and how it supports 
+                the Object-Oriented pattern, with a focus 
+                on the unique aspect of how JavaScript 
+                approaches OO. We'll explore a brief 
+                introduction to the jQuery library, 
+                which is widely used to do in-browser 
+                manipulation of the Document Object 
+                Model (DOM) and event handling. 
+                You'll also learn more about JavaScript 
+                Object Notation (JSON), which is commonly 
+                used as a syntax to exchange data between 
+                code running on the server (i.e. in PHP) 
+                and code running in the browser 
+                (JavaScript/jQuery).
+        `,
+        "learn": [
+            "Learn new concepts from industry experts",
+            "Gain a foundational understanding of a subject or tool",
+            "Develop job-relevant skills with hands-on projects",
+            "Earn a shareable career certificate"
+        ],
+        "modules": [
+            "Introduction to JavaScript",
+            "JavaScript Objects",
+            "Using JQuery",
+            "JSON - JavaScript Object Notation"
+        ],
+        "instructors": [
+            {
+                "name": "Charles Russell Severance",
+                "image": ""
+            }
+        ]
+    },
+
+    // php course details
+    {
+        "id": 4,
+        "course_name": "PHP Programming",
+        "course_label": "Complete java full course",
+        "course_image": "../assets/pic/skill-guide-php.png",
+        "prices": 89.0,
+        "descp": `
+                In this course, you'll explore the basic 
+                structure of a web application, and how 
+                a web browser interacts with a web server. 
+                You'll be introduced to the 
+                request/response cycle, including 
+                GET/POST/Redirect. You'll also gain 
+                an introductory understanding of Hypertext 
+                Markup Language (HTML), as well as the 
+                basic syntax and data structures of the 
+                PHP language, variables, logic, iteration, 
+                arrays, error handling, and superglobal 
+                variables, among other elements. 
+                An introduction to Cascading Style Sheets 
+                (CSS) will allow you to style markup for 
+                webpages. Lastly, you'll gain the skills 
+                and knowledge to install and use an 
+                integrated PHP/MySQL environment 
+                like XAMPP or MAMP.
+        `,
+        "learn": [
+            "Learn new concepts from industry experts",
+            "Gain a foundational understanding of a subject or tool",
+            "Develop job-relevant skills with hands-on projects",
+            "Earn a shareable career certificate"
+        ],
+        "modules": [
+            "Introduction to PHP",
+            "Fundamentals of PHP - Data Types",
+            "PHP Arrays",
+            "PHP Objects",
+            "Connecting PHP and MySQL",
+            "PHP Cookies and Sessions",
+            "PHP Redirect, Routing, and Authentication",
+            "Building a CRUD Application"
+        ],
+        "instructors": [
+            {
+                "name": "Charles Russell Severance",
+                "image": ""
+            },
+            {
+                "name" : "instructor 2",
+                "image": ""
+            }
+        ]
+    },
+
+    // web development course details
+    {
+        "id": 5.0,
+        "course_name": "Web Development",
+        "course_label": "HTML, CSS, and Javascript for Web Developers",
+        "course_image": "../assets/pic/Java-BCG-Banner.png",
+        "prices": 199,
+        "descp": `
+            we will learn the basic tools that every web page coder
+            needs to know. We will start from the ground up by learning
+            how to implement modern web pages with HTML and CSS. 
+        `,
+        "learn": [
+            "modern Web Development fundamentals as well as advanced topics.",
+            "Develop professional skills that position you at the top of the candidate list.",
+            "give you the foundation and logic for communicating with programming languages.",
+            "Build web developer skills."
+        ],
+        "modules": [
+            "Introduction to HTML5",
+            "Introduction to CSS3",
+            "Coding with Simple Project",
+            "Introduction to JavaScript",
+            "Using JavaScript to build Web Application"
+        ],
+        "instructors": [
+            {
+                "name": "Yaakov Chaikin",
+                "image": ""
+            }
+        ]
+    }
+];
+
+
+const course_detail_container = document.querySelector(".course-details");
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id');
+
+console.log('Received ID:', id);
+
+course_detail_container.innerHTML = `
+<!-- course details  -->
+            <div class="col-12 image-wrap p-3 border-bottom">
+                <!-- image  -->
+                <div class="img" style="background: url(${course_detail[id].image});
+                                        background-position: center;
+                                        background-size: cover;
+                                        background-repeat: no-repeat;"></div>
+            </div>
+
+            <!-- course content wrap  -->
+            <div class="col-12 course-content-wrap p-3">
+
+                <div class="row d-flex align-items-start justify-content-center  course-content-details">
+                    <div class="col-6 d-flex flex-column justify-content-start title">
+                        <h4>${course_detail[id].name}</h4>
+                        <span>Complete Java Full Course</span>
+                    </div>
+                    <div class="col-6 d-flex flex-column justify-content-end align-items-end price">
+                        <h5>Price: <span>89.0</span></h5>
+                    </div>
+                </div>
+
+                <div class="mb-5"></div>
+                <!-- description  -->
+                <div class="row d-flex align-items-start course-content-description">
+                    <div class="col-12">
+                        <h4>Description</h4>
+                        <p>
+                            Learn the Skills Needed to be a Java Programmer.
+                            Quickly master the Java programming language and
+                            the packages that constitute its rich set of core libraries.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mb-3"></div>
+                <!-- what will you learn  -->
+                <div class="row d-flex align-items-start course-content-what">
+                    <div class="col-12">
+                        <h4 class="mb-3">What will you learn?</h4>
+                        
+                        <div class="row mb-2">
+                            <div class="col-1 check-icon"><i class="fa-solid fa-check"></i></div>
+                            <div class="col-11 what-will-learn">
+                                <span>Learn the basic syntax and functions of the Java programming language</span>
+                            </div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-1 check-icon"><i class="fa-solid fa-check"></i></div>
+                            <div class="col-11 what-will-learn">
+                                <span>Apply object-oriented programming techniques to building classes,
+                                    creating objects, and understanding how solutions are packaged in Java.</span>
+                            </div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-1 check-icon"><i class="fa-solid fa-check"></i></div>
+                            <div class="col-11 what-will-learn">
+                                <span>Learn how to implement inheritance and polymorphism in Java.</span>
+                            </div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-1 check-icon"><i class="fa-solid fa-check"></i></div>
+                            <div class="col-11 what-will-learn">
+                                <span>Use selected parts of the vast Java SE class library to enhance
+                                    your Java programming techniques.</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="mb-3"></div>
+                <!-- Details to know  -->
+                <div class="row d-flex align-items-start course-content-what">
+                    <div class="col-12">
+
+                        <h4 class="mb-3">Details to Know</h4>
+                        <div class="row">
+                            <div class="col-6">
+                                <h4><i class="fa-solid fa-list-check"></i></h4>
+                                <span>Quizzes & Assessments</span>
+                            </div>
+                            <div class="col-6">
+                                <h4><i class="fa-solid fa-message"></i></h4>
+                                <span>English, Spanish, +6</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="mb-5"></div>
+                <!-- modules in this course  -->
+                <div class="row d-flex align-items-start course-content-what">
+                    <div class="col-12">
+                        <h4 class="mb-4">Modules in this course!</h4>
+                        
+                        <div class="row mb-2 course-modules-wrapper">
+                            <div class="col-1 list-number">
+                                <span class="me-2">1</span>
+                            </div>
+                            <div class="col-11 what-will-learn2">
+                                <span>Introduction to Java Programming</span>
+                            </div>
+                        </div>
+                        <div class="row mb-2 course-modules-wrapper">
+                            <div class="col-1 list-number">
+                                <span class="me-2">2</span>
+                            </div>
+                            <div class="col-11 what-will-learn2">
+                                <span>Java Languages Fundamental - Types</span>
+                            </div>
+                        </div>
+                        <div class="row mb-2 course-modules-wrapper">
+                            <div class="col-1 list-number">
+                                <span class="me-2">3</span>
+                            </div>
+                            <div class="col-11 what-will-learn2">
+                                <span>Java Languages Fundamental - Flows of Control</span>
+                            </div>
+                        </div>
+                        <div class="row mb-2 course-modules-wrapper">
+                            <div class="col-1 list-number">
+                                <span class="me-2">4</span>
+                            </div>
+                            <div class="col-11 what-will-learn2">
+                                <span>Classes and Objects</span>
+                            </div>
+                        </div>
+                        <div class="row mb-2 course-modules-wrapper">
+                            <div class="col-1 list-number">
+                                <span class="me-2">5</span>
+                            </div>
+                            <div class="col-11 what-will-learn2">
+                                <span>Creating Classes, Objects and using it</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-5"></div>
+                <!-- modules in this course  -->
+                <div class="row d-flex align-items-start course-content-what">
+                    <div class="col-12">
+                        <h4 class="mb-4">Instructors</h4>
+                        
+                        <div class="row mb-2 course-modules-wrapper2">
+                            <div class="col-4 d-flex align-items-center justify-content-center flex-column">
+                                <div class="insturct-img mb-3"></div>
+                                <h5>Instructor</h5>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="mb-3"></div>
+
+                <button class="enroll me-3">Enroll Now</button>
+                <button class="back">Back</button>
+
+                <div class="mb-5"></div>
+                <div class="mb-5"></div>
+                <div class="mb-5"></div>
+            </div>
+`;
+
