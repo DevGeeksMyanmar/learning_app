cb588cf003b457862e3708641a500592ad4c800d
Woody <waiyanwoody@gmail.com>
2023-08-20 14:42:06 +0630

    top categories added

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -9,11 +9,12 @@
 
         ScrollReveal().reveal('nav ul',{delay:500 , origin : 'top',interval : 100}); 
         ScrollReveal().reveal('.title-1,.title-2,.title-3,.learn-desc,.btn-learn-now',{ delay : 500 , origin : 'left',interval : 200});
-        ScrollReveal().reveal('.img-learn',{delay : 700 , origin : 'bottom'});
-        ScrollReveal().reveal('.btn-auth',{delay : 500 , origin : 'right'});
-        ScrollReveal().reveal('.navbar-brand',{delay : 500 , origin : 'left'});
+        ScrollReveal().reveal('.img-learn,.session-popular .title',{delay : 700 , origin : 'bottom'});
+        ScrollReveal().reveal('.btn-auth,.session-popular .swiper',{delay : 600 , origin : 'right'});
+        ScrollReveal().reveal('.navbar-brand ,.categories-title',{delay : 500 , origin : 'left'});
         ScrollReveal().reveal('.site-div',{delay : 500 , origin : 'bottom'})
         ScrollReveal().reveal('.session-info .bg-secondary',{delay : 800 , origin : 'bottom',});
+        ScrollReveal().reveal('.category-box',{delay : 600 , origin : 'bottom',interval:100,});
         ; 
 
         // ScrollReveal().reveal('.learn-desc',{delay : 500 , origin : 'bottom'});
