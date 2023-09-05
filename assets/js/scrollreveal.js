2a0c2985a726965461f41b38272458c425e5a353
Woody <waiyanwoody@gmail.com>
2023-09-05 19:06:48 +0630

    update sep4

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -14,19 +14,18 @@
         ScrollReveal().reveal('.title-1,.title-2,.title-3,.learn-desc,.btn-learn-now,.contact-left,.about-us-title,.goals h4,.collab-title',{ delay : 500 , origin : 'left',interval : 200});
         ScrollReveal().reveal('.img-learn-2,.session-popular .title,.about-us-text,.goals p',{delay : 700 , origin : 'bottom',distance : '50px'});
         ScrollReveal().reveal('.goals img,.wedo',{delay : 500 , origin : 'bottom',distance : '70px',interval:200});
-        ScrollReveal().reveal('.btn-auth,.session-popular .swiper,.contact-right',{delay : 600 , origin : 'right',mobile:false});
         ScrollReveal().reveal('.navbar-brand ,.session-title,.img-certificate',{delay : 700 , origin : 'left'});
-        ScrollReveal().reveal('.site-div,',{delay : 500 , origin : 'bottom'})
         ScrollReveal().reveal('.site-div,.team img,.copyright-text-1,.copyright-text-2',{delay : 500 ,distance:'40px', origin : 'bottom',interval:200})
 
         ScrollReveal().reveal('.session-info .bg-secondary,',{delay : 800 , origin : 'bottom',distance:'50px'});
         ScrollReveal().reveal('.category-box',{delay : 600 , origin:'bottom',interval:100,});
-        ScrollReveal().reveal('.review-box,.certificate-title,.certificate-text,.btn-enroll,.img-learn',{delay : 700 , origin:'right',interval:100,mobile:false});
         ScrollReveal().reveal('.sponsor-container',{delay:500 , distance:'20px', opacity: 0,interval : 100}); 
 
-        ScrollReveal().reveal('.btn-auth,.session-popular .swiper,.contact-right,.img-learn,.img-learn-2',{delay : 600 , origin : 'bottom',distance:'50px',desktop:false});
-        ScrollReveal().reveal('.review-box,.certificate-box',{delay : 700 , origin:'bottom',distance:'50px',interval:100,desktop:false});
+        ScrollReveal().reveal('.session-popular .swiper,.contact-right,.img-learn,.img-learn-2',{delay : 600 , origin : 'bottom',distance:'70px'});
+        ScrollReveal().reveal('.review-box,.certificate-box',{delay : 700 , origin:'bottom',distance:'70px',interval:100});
+
 
         ; 
 
     
+        
